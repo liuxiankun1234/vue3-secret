5d12040c2c30c6e7d6cc3e27de98dab5595a3126
liuxiankun <liuxiankun@zhangyue.com>
2025-06-25 11:22:48 +0800

    feat: 初始化

---
+++ notes/1.js
@@ -0,0 +1,5 @@
+// function createAppAPI<HostElement>(
+//     render: RootRenderFunction<HostElement>,
+//     hydrate?: RootHydrateFunction,
+//   ): CreateAppFunction<HostElement> {}
+// 这个ts语法了解一下
